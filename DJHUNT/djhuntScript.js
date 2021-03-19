339f58a8de8400b0c3fe958b4a62040093d35e2f
shaunpua <svshaunpuax1@gmail.com>
2021-03-19 23:41:30 +0800

    added voting functionality for dj vote

--- DJHUNT/djhuntScript.js
+++ DJHUNT/djhuntScript.js
@@ -1,3 +1,5 @@
+
+
 /*----- Open Menu -----*/
 function openNav() {
     document.getElementById("sideMenu").style.width = "250px";
@@ -12,14 +14,14 @@ function closeNav() {
 
 /*----- Confirm Vote -----*/
 function openConfirm() {
-    document.getElementById("overlay").style.display = "block";
+    document.getElementById("djvote-overlay").style.display = "block";
     document.getElementById("voteConfirmed").style.display = "none";
     document.getElementById("confirmation").style.display = "block";
 }
 
 /*----- Cancel Vote -----*/
 function cancelVote() {
-    document.getElementById("overlay").style.display = "none";
+    document.getElementById("djvote-overlay").style.display = "none";
     document.getElementById("voteConfirmed").style.display = "none";
     document.getElementById("confirmation").style.display = "none";
 }
@@ -28,7 +30,7 @@ function cancelVote() {
 
 /*----- Close Overlay -----*/
 function closeConfirm() {
-    document.getElementById("overlay").style.display = "none";
+    document.getElementById("djvote-overlay").style.display = "none";
     document.getElementById("voteConfirmed").style.display = "none";
     document.getElementById("confirmation").style.display = "none";
 }
@@ -59,3 +61,105 @@ function openTab(evt, tabName) {
         evt.currentTarget.className += " active";
     }
 }
+
+/*----- selecting dj card -------*/
+
+
+
+  
+
+
+function voteDj(){
+    const votedjbutton = document.getElementsByClassName('round')
+    //const votedjbutton = document.querySelectorAll("input[type=checkbox][name=dj-check]");
+    //console.log(votedjbutton);
+
+    
+    for (let i = 0; i < votedjbutton.length; i++) {
+        let button = votedjbutton[i]
+        
+        buttonCheck = button.children[0]
+        
+        //console.log(button)
+        //console.log(buttonCheck)
+        
+        //buttonCheck.addEventListener('change', addDj)
+
+        buttonCheck.addEventListener('change', (event) => {
+            if (event.currentTarget.checked) {
+                addDj(event);
+                console.log('checked');
+            } else {
+                removeDj(event);
+                console.log('not checked');
+            }
+        })
+
+        
+       
+    }
+
+}
+
+function addDj(event){
+    //event.preventDefault();
+    console.log(button);
+    var button = event.target;
+    var addedDj = button.parentElement.parentElement;
+    console.log(addedDj)
+    var djImage = addedDj.getElementsByClassName('dj-background')[0].src;
+    var djName = addedDj.getElementsByClassName('dj-name')[0].innerText;
+    
+    createVotedDj(djName, djImage);
+
+}
+
+function createVotedDj(name, image){
+    var djColumn = document.createElement('div');
+    djColumn.classList.add('votedDjCard');
+    djCard = document.getElementsByClassName('votedDjs')[0];
+
+    var djCardContent = `<div class="votedDjCard">
+    <div class="votedBackground">
+        <img class="votedImage"src="${image}" >
+    </div>
+    <h2 class="votedName">${name}</h2>
+    <p class="votedTagline">Tagline</p> 
+    </div>`
+    console.log(djCard)
+    console.log(djColumn)
+    djColumn.innerHTML = djCardContent;
+    djCard.append(djColumn);
+
+}
+
+function removeDj(event){
+    var buttonClicked = event.target
+    btnParent =  buttonClicked.parentElement.parentElement;
+    votedDjname = btnParent.getElementsByClassName('dj-name')[0].innerText;
+
+
+    removedDjs = document.querySelectorAll('.votedDjCard');
+
+    for (let i = 0; i < removedDjs.length; i++){
+        
+        removedDj= removedDjs[i];
+
+        removedName = removedDj.getElementsByClassName('votedName')[0].innerText;
+
+        if (removedName === votedDjname) {
+            removedDj.remove();
+        }
+
+    }
+
+    //  console.log(removedDjs)
+
+
+    //console.log(votedDjname);
+}
+
+// use as a an arugment to find 
+
+voteDj();
+
