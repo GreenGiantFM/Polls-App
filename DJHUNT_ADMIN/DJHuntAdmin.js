cdda402033220111b96d6e211248d868fa665786
ndlngo-dlsu <nathan_ngo@dlsu.edu.ph>
2021-05-15 20:17:20 +0800

    ADMIN DJ HUNT (Add Tab)

--- DJHUNT_ADMIN/DJHuntAdmin.js
+++ DJHUNT_ADMIN/DJHuntAdmin.js
@@ -503,3 +503,13 @@ $(window).on('scroll', function () {
         }
     }
 });
+
+/*show Image Preview*/
+function showPreview(event){
+    if(event.target.files.length > 0){
+      var src = URL.createObjectURL(event.target.files[0]);
+      var preview = document.getElementById("file-ip-1-preview");
+      preview.src = src;
+      preview.style.display = "block";
+    }
+  }
