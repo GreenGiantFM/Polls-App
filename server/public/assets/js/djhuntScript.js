9f723379c0852eca8bcc53c666e565f8b6c53bd8
bernicebetito <bernice_betito@dlsu.edu.ph>
2021-07-02 22:53:04 +0800

    google sign in button

--- server/public/assets/js/djhuntScript.js
+++ server/public/assets/js/djhuntScript.js
@@ -247,6 +247,14 @@ $(document).ready(() => {
             })
         }
     })
+
+    if (window.innerWidth >= 350) {
+        $('#deskSignIn').css("display", "block");
+        $('#mobileSignIn').css("display", "none");
+    } else if (window.innerWidth <= 349) {
+        $('#deskSignIn').css("display", "none");
+        $('#mobileSignIn').css("display", "block");
+    }
 })
 
 /*----- Open selected Tab -----*/
@@ -726,6 +734,7 @@ function submitVote(){
                     document.getElementById("voteSubmitted").style.display = "grid";
                     document.getElementById("voteTab").style.display = "none";
                     document.getElementById("liveTab").style.display = "none";
+                    document.getElementById("djSignIn").style.display = "none";
                     document.body.style.background = '#aad68a';
                     document.body.style.background = "linear-gradient(to bottom, #f1faeb, #569429)";
                 
